481597679be57e899e432f5421c974a0611470ba
Chandan <chandan031999@gmail.com>
2021-09-06 18:15:23 +0530

    As Part of SomeSTory

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo{
+
+public static void main(String[]args){
+
+System.out.println("Chandan Shabnam");
+}
+
+public void add(){
+
+int a=20;
+int b=30;
+System.out.println("The Addtion is -"+(a+b));
+
+}
+}
