f99a7ffb90c28169c4ba4725e273c7d1e89d7abf
Chandan <chandan031999@gmail.com>
2021-09-07 11:38:29 +0530

    added Varaible

--- Demo.java
+++ Demo.java
@@ -3,13 +3,16 @@ public class Demo{
 public static void main(String[]args){
 
 System.out.println("Chandan Shabnam");
+
+
+Demo d=new Demo();
+d.add(12,49);
 }
 
-public void add(){
-
-int a=20;
-int b=30;
+public void add(int a,int b){
 System.out.println("The Addtion is -"+(a+b));
+
+
 
 }
 }
